3557b253e99b32a2caca8285c124930ebf06156b
KaizenSheesh <anjasmoonton@gmail.com>
2024-11-09 22:59:27 +0700

    fix: decorators

--- siperpus/register/decorators.py
+++ siperpus/register/decorators.py
@@ -1,12 +1,12 @@
-# decorators.py
-from functools import wraps
-from django.shortcuts import redirect
+from django.http import JsonResponse
 from .session_auth import SessionAuth
 
-def login_required(view_func):
-    @wraps(view_func)
-    def wrapper(request, *args, **kwargs):
-        if not SessionAuth.get_current_user(request):
-            return redirect('login')
-        return view_func(request, *args, **kwargs)
-    return wrapper
+def role_required(required_role):
+    def decorator(view_func):
+        def wrapper(request, *args, **kwargs):
+            current_user = SessionAuth.get_current_user(request)
+            if not current_user or current_user['role'] != required_role:
+                return JsonResponse({'error': 'Unauthorized access'}, status=403)
+            return view_func(request, *args, **kwargs)
+        return wrapper
+    return decorator
