d02c25221ca75cb5d939bf649268df7e3f1dc1fd
KaizenSheesh <anjasmoonton@gmail.com>
2024-11-09 22:57:50 +0700

    fix: fix url for login and register

--- siperpus/register/urls.py
+++ siperpus/register/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('admin/add_staff/', views.add_staff, name='add_staff'),
     path('', views.home_view, name='home'),
 ]
