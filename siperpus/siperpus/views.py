a11d865e00b6aa1bfc85b1bb44338be805eed5ac
KaizenSheesh <anjasmoonton@gmail.com>
2024-12-04 15:34:04 +0700

    add: url for path books

--- siperpus/siperpus/views.py
+++ siperpus/siperpus/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import json
+import os
+
+BOOKS_FILE = os.path.join(os.path.dirname(__file__), '../books_api.')
 
 def homepage(request):
     return render(request, 'home.html')
