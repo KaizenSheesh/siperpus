5423546c9db6066f90a83f016b2693077582c2e6
KaizenSheesh <anjasmoonton@gmail.com>
2024-12-04 15:34:18 +0700

    fix: url for path books

--- siperpus/siperpus/views.py
+++ siperpus/siperpus/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-import json
-import os
-
-BOOKS_FILE = os.path.join(os.path.dirname(__file__), '../books_api.')
 
 def homepage(request):
     return render(request, 'home.html')
